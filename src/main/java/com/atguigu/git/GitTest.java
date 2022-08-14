fc02bdc800ba387307f5191d222147dc1adb50b1
Remia <lzw@qq.com>
2022-08-14 11:12:18 +0800

    master commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -6,6 +6,9 @@ public class GitTest {
         System.out.println("hello git2!");
         System.out.println("hello git3!");
         System.out.println("hello git4!");
+        System.out.println("hello git5!");
+        System.out.println("hello git6!");
+        System.out.println("hello git7!");
         System.out.println("hello git hot-fix 1");
         System.out.println("hello git hot-fix 2");
 
